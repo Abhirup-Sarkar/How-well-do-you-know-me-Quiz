68ec901ebcb9f68a925967c3a978361581cf597d
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-06-09 22:38:43 +0530

    added highscore

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ function play(question,correctAnswer)
 
 }
 //QuestionList
-var questionList = [
+var questionList =
+ [
     {
         question: "1. Where do you live? ",
         a: "[a]: Serampore",
@@ -55,5 +56,46 @@ var questionList = [
         c: "[c]: Biriyani",
         correctAnswer: "b",
     },
+    {
+        question: "4. What is my favorite color? ",
+        a: "[a]: Black",
+        b: "[b]: Blue",
+        c: "[c]: Pink",
+    },
+]   
+//calling the function
+for(i=0;i<questionList.length;i++)
+{
+    var currentQuestion = questionList[i].question;
+    var optionOne = questionList[i].a;
+    var optionTwo = questionList[i].b;
+    var optionThree = questionList[i].c;
+    console.log(currentQuestion);
+    console.log(optionOne);
+    console.log(optionTwo);
+    console.log(optionThree);
+
+    play(currentQuestion.question,currentQuestion.correctAnswer);
+}
+console.log("Well Done!");
+console.log("Your final score: ",score);
+console.log("------------------");
+//Scoreboard
+var highScore ={
+    name: "dummyName",
+    score: 2,
+} 
+if(score>=highScore.score)
+{
+    highScore.score = score;
+    console.log("The highest score, "+highScore.score+", is made by you"+userName);
+
+}
+else
+{
+    console.log("Too bad! You could not beat the higest score");
+    console.log("the highest score is: ",highScore);
+}
+
+
     
-]
